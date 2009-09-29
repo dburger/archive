7a870b3b25b84626a25481a66fe96e40f67f3c03
David Burger <davidjburger@yahoo.com>
2009-09-28 14:31:06 -1000

    add some scripts from the old notebook

--- langs/cli/examples.sh
+++ langs/cli/examples.sh
@@ -6,3 +6,22 @@ ls QuizUI_* | sed 's/Quiz\(UI_[0-9][0-9]\.png\)/mv Quiz\1 \1/' | sh
 # remove from { to } in a text file spanning multiple lines, used to clean up
 # an informix DDL for usage with SQL Server, derived from sed one liners page
 sed -e ':a;s/{[^}]*}//g;/{/N;/{/ba' input.ddl
+
+# mail with sendmail compatible send
+mail -s 'pagemon report' to@addr.com -- -F'David J. Burger' --ffrom@addr.com"
+
+# simple loop
+for i in *; do ....; .....; done
+
+# remove files from a directory older than 30 days
+find /home/you/backup/ -mtime +30 -exec rm {} \;
+
+# sort a CSV file by a column, here by column 7
+sort -t',' -k 7 hotspot-list2.txt  > foo.txt
+
+# set up a tunnel, local box listening on lport hitting rport on tohost,
+# through throughhost
+ssh -L  lport:tohost:rport throughhost
+
+# chown a symlink, pass -h
+chown -h user:group symlink
