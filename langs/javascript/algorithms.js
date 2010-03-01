e008a96a6de4d6ad4fb9c4899ad8dd6be96cf237
David J. Burger <dburger@dell-wildcat.gateway.2wire.net>
2010-02-28 16:42:12 -0800

    starting a file of javascript alogirhtm code kata

---
+++ langs/javascript/algorithms.js
@@ -0,0 +1,21 @@
+// collection of javascript algorithms...mainly mini fun code kata
+
+// function to reverse linked list, given we have node objects with next field
+// and we have a reference to the head node
+function reverse(node, last) {
+  // say last node should have next === null, not undefined
+  last = last || null;
+  var next = node.next;
+  node.next = last;
+  return (next) ? reverse(next, node) : node;
+}
+
+// in place randomization of an array
+function shuffle(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+  }
+}
